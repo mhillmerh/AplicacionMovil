4b14e6209d4fa80358d74a99bb31744ca335e302
mhillmerh <m.hillmerh@gmail.com>
2022-09-26 14:12:19 -0300

    Cambiosslides

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+  
 
   constructor() { }
 
   ngOnInit() {
   }
   
+ 
+}
+
+export class imageSlider{
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400,
+    loop: true,
+    autoplay: {
+          delay: 400
+    }
+  };
+  constructor() {}
+
+
+
 
 }
+
+
